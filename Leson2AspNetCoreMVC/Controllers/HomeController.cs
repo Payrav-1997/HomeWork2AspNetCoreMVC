fb48675274dcaeed3a43e68141f263117ba8e3e3
User <payrav-343@mail.ru>
2020-05-24 01:10:47 +0500

    Добавьте файлы проекта.

---
+++ Leson2AspNetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Leson2AspNetCoreMVC.Models;
+
+
+namespace Leson2AspNetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        
+
+        
+    }
+}
---
+++ Leson2AspNetCoreMVC/Controllers/PersonController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Dapper;
+using System.Data;
+using Leson2AspNetCoreMVC.Models;
+
+namespace Leson2AspNetCoreMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        private string conString = "Data Source=localhost;Initial Catalog=Person;Integrated Security=True";
+        
+        [HttpGet]
+        
+        public IActionResult Index()
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(conString))
+                {
+                    var personList = db.Query<Person>("SELECT * FROM PERSON");
+                    return View(personList);
+                }
+            }
+            catch 
+            {
+
+                
+            }
+            return View(null);
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Add(string LastName, string FirstName, string MiddleName)
+        {
+            var person = new Person()
+            {
+                LastName = LastName,
+                FirstName= FirstName,
+                MiddleName= MiddleName
+            };
+            using(IDbConnection db = new SqlConnection(conString))
+            {
+                db.Execute("INSERT INTO PERSON([LastName],[FirstName],[MiddleName])+" +
+                    $"VALUES'{person.LastName}','{person.FirstName}','{person.MiddleName}'");         
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
